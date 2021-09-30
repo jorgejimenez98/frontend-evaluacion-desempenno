dda34ddc446a857e1cd07a11489c25308f946ef3
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-09-30 02:48:54 +0200

    ADDED config header to actions

--- src/redux/actions/dashboardActions.js
+++ src/redux/actions/dashboardActions.js
@@ -3,14 +3,26 @@ import { NUMBERS } from "../constants/dashboardConstants";
 import axios from "axios";
 import { defaultApi } from "src/publicUrl";
 
-export const getMainNumbers = () => async (dispatch) => {
+export const getMainNumbers = () => async (dispatch, getState) => {
   try {
     dispatch({
       type: NUMBERS.GET_DATA,
     });
 
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
     const { data } = await axios.get(
       `${defaultApi}/api/getMainNumbers/`,
+      config
     );
 
     dispatch({
