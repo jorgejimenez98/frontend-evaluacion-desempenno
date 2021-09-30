9f4e141c48fd43fc27872e83dd9d8dba9176ab40
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-09-30 04:24:36 +0200

    UPDATED dashboard component

--- src/views/screens/HomeScreen.js
+++ src/views/screens/HomeScreen.js
@@ -1,22 +1,13 @@
 import React, { lazy, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Loader, Message } from "src/containers/utils";
+import { CCard, CCardBody, CCardGroup, CCardHeader } from "@coreui/react";
+import { CChartPie, CChartDoughnut } from "@coreui/react-chartjs";
 import {
-  CButton,
-  CButtonGroup,
-  CCard,
-  CCardBody,
-  CCardFooter,
-  CCardGroup,
-  CCardHeader,
-  CCol,
-  CProgress,
-  CRow,
-} from "@coreui/react";
-import { CChartBar, CChartPie } from "@coreui/react-chartjs";
-import CIcon from "@coreui/icons-react";
-import MainChartExample from "../charts/MainChartExample.js";
-import { getMainNumbers } from "src/redux/actions/dashboardActions";
+  getMainNumbers,
+  getRangeEvaluation,
+  getAnualRangeEvaluation,
+} from "src/redux/actions/dashboardActions";
 
 const WidgetsDropdown = lazy(() => import("../widgets/WidgetsDropdown.js"));
 
@@ -37,13 +28,29 @@ const HomeScreen = ({ history }) => {
     error: errorNumbers,
   } = useSelector((state) => state.numbers);
 
+  // Evaluation Range Selector
+  const {
+    loading: loadingEvaluations,
+    error: errorEvaluations,
+    evaluationRange,
+  } = useSelector((state) => state.evaluationRange);
+
+  // Evaluation Anual Range Selector
+  const {
+    loading: loadingAnualEvaluations,
+    error: errorAnualEvaluations,
+    evaluationRange: evaluationAnualRange,
+  } = useSelector((state) => state.evaluationAnualRange);
+
   useEffect(() => {
     if (!userInfo) {
       history.push("/login");
     } else {
       dispatch(getMainNumbers());
+      dispatch(getRangeEvaluation());
+      dispatch(getAnualRangeEvaluation());
     }
-  }, [userInfo, history]);
+  }, [userInfo, history, dispatch]);
 
   return (
     <React.Fragment>
@@ -60,130 +67,72 @@ const HomeScreen = ({ history }) => {
         />
       )}
 
-      <CCard>
-        <CCardBody style={{ fontSize: "" }}>
-          <CRow>
-            <CCol sm="5">
-              <h4 id="traffic" className="card-title mb-0">
-                Traffic
-              </h4>
-              <div className="small text-muted">November 2017</div>
-            </CCol>
-            <CCol sm="7" className="d-none d-md-block">
-              <CButton color="primary" className="float-right">
-                <CIcon name="cil-cloud-download" />
-              </CButton>
-              <CButtonGroup className="float-right mr-3">
-                {["Day", "Month", "Year"].map((value) => (
-                  <CButton
-                    color="outline-secondary"
-                    key={value}
-                    className="mx-0"
-                    active={value === "Month"}
-                  >
-                    {value}
-                  </CButton>
-                ))}
-              </CButtonGroup>
-            </CCol>
-          </CRow>
-          <MainChartExample style={{ height: "300px", marginTop: "40px" }} />
-        </CCardBody>
-        <CCardFooter>
-          <CRow className="text-center">
-            <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">Visits</div>
-              <strong>29.703 Users (40%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="success"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-              <div className="text-muted">Unique</div>
-              <strong>24.093 Users (20%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="info"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">Pageviews</div>
-              <strong>78.706 Views (60%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="warning"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">New Users</div>
-              <strong>22.123 Users (80%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="danger"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-              <div className="text-muted">Bounce Rate</div>
-              <strong>Average Rate (40.15%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                value={40}
-              />
-            </CCol>
-          </CRow>
-        </CCardFooter>
-      </CCard>
-
       <CCardGroup columns className="cols-2">
-        <CCard>
-          <CCardHeader>Bar Chart</CCardHeader>
-          <CCardBody>
-            <CChartBar
-              datasets={[
-                {
-                  label: "GitHub Commits",
-                  backgroundColor: "#f87979",
-                  data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
-                },
-              ]}
-              labels="months"
-              options={{
-                tooltips: {
-                  enabled: true,
-                },
-              }}
-            />
-          </CCardBody>
-        </CCard>
+        {loadingAnualEvaluations ? (
+          <Loader />
+        ) : errorAnualEvaluations ? (
+          <Message variant="danger">{errorAnualEvaluations}</Message>
+        ) : (
+          evaluationAnualRange && (
+            <CCard className="shadow">
+              <CCardHeader className="text-center">
+                Rango de Evaluaciones Anuales
+              </CCardHeader>
+              <CCardBody>
+                <CChartDoughnut
+                  datasets={[
+                    {
+                      backgroundColor: ["#41B883", "#00D8FF", "#DD1B16"],
+                      data: evaluationAnualRange,
+                    },
+                  ]}
+                  labels={["Superior", "Adecuado", "Deficiente"]}
+                  options={{
+                    tooltips: {
+                      enabled: true,
+                    },
+                  }}
+                />
+              </CCardBody>
+            </CCard>
+          )
+        )}
 
-        <CCard>
-          <CCardHeader>Pie Chart</CCardHeader>
-          <CCardBody>
-            <CChartPie
-              datasets={[
-                {
-                  backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
-                  data: [40, 20, 80, 10],
-                },
-              ]}
-              labels={["VueJs", "EmberJs", "ReactJs", "AngularJs"]}
-              options={{
-                tooltips: {
-                  enabled: true,
-                },
-              }}
-            />
-          </CCardBody>
+        <CCard className="shadow">
+          {loadingEvaluations ? (
+            <Loader />
+          ) : errorEvaluations ? (
+            <Message variant="danger">{errorEvaluations}</Message>
+          ) : (
+            evaluationRange && (
+              <div>
+                <CCardHeader className="text-center">
+                  Rango de Evaluaciones Mensuales de Melya
+                </CCardHeader>
+                <CCardBody>
+                  <CChartPie
+                    datasets={[
+                      {
+                        backgroundColor: [
+                          "#41B883",
+                          "#00D8FF",
+                          "#adff2f",
+                          "#DD1B16",
+                        ],
+                        data: evaluationRange,
+                      },
+                    ]}
+                    labels={["Muy Bien", "Bien", "Regular", "Mal"]}
+                    options={{
+                      tooltips: {
+                        enabled: true,
+                      },
+                    }}
+                  />
+                </CCardBody>
+              </div>
+            )
+          )}
         </CCard>
       </CCardGroup>
     </React.Fragment>
