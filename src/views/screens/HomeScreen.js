976c472ebec855efb9fc954c836ad1886dd70b7b
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-09-30 02:15:48 +0200

    ADDED main number on home screens

--- src/views/screens/HomeScreen.js
+++ src/views/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { lazy, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { Loader, Message } from "src/containers/utils";
 import {
   CButton,
   CButtonGroup,
@@ -11,122 +12,141 @@ import {
   CCol,
   CProgress,
   CRow,
-  CWidgetProgressIcon,
 } from "@coreui/react";
 import { CChartBar, CChartPie } from "@coreui/react-chartjs";
-import { DocsLink } from "src/reusable";
 import CIcon from "@coreui/icons-react";
 import MainChartExample from "../charts/MainChartExample.js";
+import { getMainNumbers } from "src/redux/actions/dashboardActions";
 
 const WidgetsDropdown = lazy(() => import("../widgets/WidgetsDropdown.js"));
 
 const HomeScreen = ({ history }) => {
+  const dispatch = useDispatch();
+
   // User Selector
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  // Numbers Selector
+  const {
+    users,
+    salePlans,
+    salePlaces,
+    families,
+    loading,
+    error: errorNumbers,
+  } = useSelector((state) => state.numbers);
+
   useEffect(() => {
     if (!userInfo) {
       history.push("/login");
+    } else {
+      dispatch(getMainNumbers());
     }
   }, [userInfo, history]);
 
   return (
     <React.Fragment>
-      <WidgetsDropdown />
+      {loading ? (
+        <Loader />
+      ) : errorNumbers ? (
+        <Message variant="danger">{errorNumbers}</Message>
+      ) : (
+        <WidgetsDropdown
+          users={String(users)}
+          salePlans={String(salePlans)}
+          salePlaces={String(salePlaces)}
+          families={String(families)}
+        />
+      )}
 
-      {
-        <CCard>
-          <CCardBody style={{ fontSize: "" }}>
-            <CRow>
-              <CCol sm="5">
-                <h4 id="traffic" className="card-title mb-0">
-                  Traffic
-                </h4>
-                <div className="small text-muted">November 2017</div>
-              </CCol>
-              <CCol sm="7" className="d-none d-md-block">
-                <CButton color="primary" className="float-right">
-                  <CIcon name="cil-cloud-download" />
-                </CButton>
-                <CButtonGroup className="float-right mr-3">
-                  {["Day", "Month", "Year"].map((value) => (
-                    <CButton
-                      color="outline-secondary"
-                      key={value}
-                      className="mx-0"
-                      active={value === "Month"}
-                    >
-                      {value}
-                    </CButton>
-                  ))}
-                </CButtonGroup>
-              </CCol>
-            </CRow>
-            <MainChartExample style={{ height: "300px", marginTop: "40px" }} />
-          </CCardBody>
-          <CCardFooter>
-            <CRow className="text-center">
-              <CCol md sm="12" className="mb-sm-2 mb-0">
-                <div className="text-muted">Visits</div>
-                <strong>29.703 Users (40%)</strong>
-                <CProgress
-                  className="progress-xs mt-2"
-                  precision={1}
-                  color="success"
-                  value={40}
-                />
-              </CCol>
-              <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-                <div className="text-muted">Unique</div>
-                <strong>24.093 Users (20%)</strong>
-                <CProgress
-                  className="progress-xs mt-2"
-                  precision={1}
-                  color="info"
-                  value={40}
-                />
-              </CCol>
-              <CCol md sm="12" className="mb-sm-2 mb-0">
-                <div className="text-muted">Pageviews</div>
-                <strong>78.706 Views (60%)</strong>
-                <CProgress
-                  className="progress-xs mt-2"
-                  precision={1}
-                  color="warning"
-                  value={40}
-                />
-              </CCol>
-              <CCol md sm="12" className="mb-sm-2 mb-0">
-                <div className="text-muted">New Users</div>
-                <strong>22.123 Users (80%)</strong>
-                <CProgress
-                  className="progress-xs mt-2"
-                  precision={1}
-                  color="danger"
-                  value={40}
-                />
-              </CCol>
-              <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-                <div className="text-muted">Bounce Rate</div>
-                <strong>Average Rate (40.15%)</strong>
-                <CProgress
-                  className="progress-xs mt-2"
-                  precision={1}
-                  value={40}
-                />
-              </CCol>
-            </CRow>
-          </CCardFooter>
-        </CCard>
-      }
+      <CCard>
+        <CCardBody style={{ fontSize: "" }}>
+          <CRow>
+            <CCol sm="5">
+              <h4 id="traffic" className="card-title mb-0">
+                Traffic
+              </h4>
+              <div className="small text-muted">November 2017</div>
+            </CCol>
+            <CCol sm="7" className="d-none d-md-block">
+              <CButton color="primary" className="float-right">
+                <CIcon name="cil-cloud-download" />
+              </CButton>
+              <CButtonGroup className="float-right mr-3">
+                {["Day", "Month", "Year"].map((value) => (
+                  <CButton
+                    color="outline-secondary"
+                    key={value}
+                    className="mx-0"
+                    active={value === "Month"}
+                  >
+                    {value}
+                  </CButton>
+                ))}
+              </CButtonGroup>
+            </CCol>
+          </CRow>
+          <MainChartExample style={{ height: "300px", marginTop: "40px" }} />
+        </CCardBody>
+        <CCardFooter>
+          <CRow className="text-center">
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">Visits</div>
+              <strong>29.703 Users (40%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="success"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
+              <div className="text-muted">Unique</div>
+              <strong>24.093 Users (20%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="info"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">Pageviews</div>
+              <strong>78.706 Views (60%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="warning"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">New Users</div>
+              <strong>22.123 Users (80%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="danger"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
+              <div className="text-muted">Bounce Rate</div>
+              <strong>Average Rate (40.15%)</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                value={40}
+              />
+            </CCol>
+          </CRow>
+        </CCardFooter>
+      </CCard>
 
       <CCardGroup columns className="cols-2">
         <CCard>
-          <CCardHeader>
-            Bar Chart
-            <DocsLink href="http://www.chartjs.org" />
-          </CCardHeader>
+          <CCardHeader>Bar Chart</CCardHeader>
           <CCardBody>
             <CChartBar
               datasets={[
