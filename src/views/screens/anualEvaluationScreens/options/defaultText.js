68bfe0a16ca08d1ec65559c3c3c7ed2fae413098
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-11 02:02:59 +0100

    CREATE default texts function

---
+++ src/views/screens/anualEvaluationScreens/options/defaultText.js
@@ -0,0 +1,9 @@
+export const detaultTexts = (year) => {
+  return {
+    resumenValorativo: `En el año que se evalúa ha demostrado buen cumplimiento de sus obligaciones y funciones en su desempeño laboral, ha tenido buenas evaluaciones, y se le han señalado en su momento las deficiencias a erradicar. Es un trabajador que siempre está dispuesto a lo que se le oriente en su desempeño como dependiente.`,
+    cumplimentoObjetivos: `Este año ${year} ha sido un año de trasformaciones en su desempeño pues con la nueva administración Meliá, tuvo que recibir los entrenamientos necesarios para poder aplicar los nuevos estándares y lograr lo que realmente se esperaba de su desempeño, participo en todas las formaciones y logró aplicarlas, ha tenido buen desempeño, no ha presentado problemas de calidad, los planes de ingreso de su área no se han cumplido, pero no dependen de su desempeño`,
+    comportamiento: `Ha cumplido con las normas de seguridad y salud del trabajo`,
+    usoYCuidado: `Necesita ser más constante con el cuidado y control de los recursos y medios bajo su área de responsabilidad material`,
+    recomendaciones: `Cumplir con todos los controles, llenar todos los modelos y especificaciones que se llevan en el área, así como aumentar su gestión de ventas para lograr mayores ingresos. Seguir superándose en el idioma y estudiar diariamente el Manuel de procedimientos del área.`,
+  };
+};
