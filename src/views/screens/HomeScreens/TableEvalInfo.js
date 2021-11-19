134e242d82e822f8aeb01643567b9832feffad2c
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-19 04:04:56 +0100

    ADD table eval info

--- src/views/screens/HomeScreens/TableEvalInfo.js
+++ src/views/screens/HomeScreens/TableEvalInfo.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { listOptions, MUIDataTable } from "src/containers/utils/index";
+import { columns } from "./options/columns";
 
-function TableEvalInfo() {
-    return (
-        <div>
-            Table
-        </div>
-    )
+function TableEvalInfo({ table }) {
+  // Remover Several Defaults Icons From Mui Datatable
+  listOptions.selectableRows = "none";
+  listOptions.download = false;
+  listOptions.print = false;
+  listOptions.viewColumns = true;
+  listOptions.filter = true;
+
+
+  // Add Buttom to insert users to the table
+  listOptions.customToolbar = () => {
+    return null;
+  };
+  return (
+    <div>
+      <MUIDataTable
+        title={`Listado de Evaluaciones (${table.length})`}
+        data={table}
+        columns={columns}
+        options={listOptions}
+      />
+    </div>
+  );
 }
 
-export default TableEvalInfo
+export default TableEvalInfo;
