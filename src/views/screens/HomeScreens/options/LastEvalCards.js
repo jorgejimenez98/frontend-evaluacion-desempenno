db17a37ab254f224018b58a5e09451818ef52bcf
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-19 19:33:51 +0100

    Add paytime to card props ordered from last to third

--- src/views/screens/HomeScreens/options/LastEvalCards.js
+++ src/views/screens/HomeScreens/options/LastEvalCards.js
@@ -1,18 +1,18 @@
 import React from "react";
 import CardEval from "./CardEval";
 
-function LastEvalCards() {
+function LastEvalCards({ paytimes }) {
   return (
     <div className="p-2 mb-2">
       <div className="row">
         <div className="col-md-4">
-          <CardEval />
+          <CardEval paytime={paytimes.first} title={"Último mes"} />
         </div>
         <div className="col-md-4">
-          <CardEval />
+          <CardEval paytime={paytimes.second} title={"Penúltimo mes"} />
         </div>
         <div className="col-md-4">
-          <CardEval />
+          <CardEval paytime={paytimes.third} title={"Antepenúltimo mes"} />
         </div>
       </div>
     </div>
