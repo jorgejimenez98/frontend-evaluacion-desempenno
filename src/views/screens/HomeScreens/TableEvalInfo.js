553b3404c6c6a35c2813b1c59688bc9a23c08666
Jorge Jimenez <georgeguitarra07@gmail.com>
2021-11-19 19:04:01 +0100

    Add card eval component to table header

--- src/views/screens/HomeScreens/TableEvalInfo.js
+++ src/views/screens/HomeScreens/TableEvalInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { listOptions, MUIDataTable } from "src/containers/utils/index";
 import { columns } from "./options/columns";
+import LastEvalCards from './options/LastEvalCards'
 
 function TableEvalInfo({ table }) {
   // Remover Several Defaults Icons From Mui Datatable
@@ -10,13 +11,14 @@ function TableEvalInfo({ table }) {
   listOptions.viewColumns = true;
   listOptions.filter = true;
 
-
   // Add Buttom to insert users to the table
   listOptions.customToolbar = () => {
     return null;
   };
+
   return (
-    <div>
+    <div className="mb-3">
+      <LastEvalCards />
       <MUIDataTable
         title={`Listado de Evaluaciones (${table.length})`}
         data={table}
